4014a81e7b3a443f520a9292c87219b3dd88d570
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-20 04:14:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,10 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |languages_type, language_name|
+    language_name.each do |
+    language_name.each do 
+    
+      
+  
 end
